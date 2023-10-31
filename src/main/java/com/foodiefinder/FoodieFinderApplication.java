6e43641fbf4ebbaa94095da553f821f3841e7c26
jeongjiwon <jungkt0x@naver.com>
2023-10-31 11:19:26 +0900

    feat: Initial project setup

---
+++ src/main/java/com/foodiefinder/FoodieFinderApplication.java
@@ -0,0 +1,13 @@
+package com.foodiefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodiefinder/FoodieFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodiefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
