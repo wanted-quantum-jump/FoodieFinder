c56bfa0cce0d72b871c0c7c8df2d0a0cd323fb84
Hyeri Jang <chjang5621@naver.com>
2023-11-01 13:06:18 +0900

    style : BaseTimeEntity 주석 추가
    클래스 설명을 추가했습니다.

--- src/main/java/com/foodiefinder/common/entity/BaseTimeEntity.java
+++ src/main/java/com/foodiefinder/common/entity/BaseTimeEntity.java
@@ -8,6 +8,12 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+/**
+ * 테이블에 createdAt, updatedAt 필드를 추가시키는 추상 클래스입니다.
+ *
+ * @author hyerijang
+ * @version 1.0
+ */
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
