e6358079330583f0a1efc89c26c34603841b6bec
Hyeri Jang <chjang5621@naver.com>
2023-11-02 11:35:02 +0900

    feat : CSV 데이터를 DB에 저장
    - 어플리케이션 실행시 최초 1회 실행됩니다.
    - 중복되는 데이터는 가장 마지막으로 나온 값으로 갱신되도록 하였습니다.
    
    #22

--- src/main/java/com/foodiefinder/common/exception/ErrorCode.java
+++ src/main/java/com/foodiefinder/common/exception/ErrorCode.java
@@ -11,6 +11,10 @@ public enum ErrorCode {
     //User
     USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "U001", "이미 계정이 존재합니다."),
 
+    //CSV
+    NOT_VALID_CSV(HttpStatus.NOT_IMPLEMENTED,"C001" ,"유효하지 않은 형식의 CSV입니다." ),
+    CSV_FILE_EXCEPTION(HttpStatus.NOT_IMPLEMENTED,"C002" ,"CSV 파일을 읽어올 수 없습니다." ),
+    NOT_VALID_FILEPATH(HttpStatus.NOT_IMPLEMENTED,"C003" , "파일 경로 혹은 파일에 문제가 있습니다."),
     ;
 
     private final HttpStatus httpStatus;
