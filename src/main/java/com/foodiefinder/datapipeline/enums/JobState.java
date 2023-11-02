0871779cf6ffff975cf44bc642576d0c6503e2df
BA <windy01999@gmail.com>
2023-11-02 12:20:45 +0900

    feat: 데이터를 가져올 url 목록, Job 의 상태 enum 클래스

---
+++ src/main/java/com/foodiefinder/datapipeline/enums/JobState.java
@@ -0,0 +1,5 @@
+package com.foodiefinder.datapipeline.enums;
+
+public enum JobState {
+    RETRY, NEXT
+}
---
+++ src/main/java/com/foodiefinder/datapipeline/enums/OpenApiUrl.java
@@ -0,0 +1,25 @@
+package com.foodiefinder.datapipeline.enums;
+
+// 사용할 url 이 있다면 추가
+public enum OpenApiUrl {
+    LAUNCH("Genrestrtlunch"), // 김밥 도시락
+    CAFE("Genrestrtcate"), // 카페
+    MNTCOOK("Genrestrtmovmntcook"), // 일반 음식점(이동 조리)
+    CHIFOOD("Genrestrtchifood"), // 중식
+    JPNFOOD("Genrestrtjpnfood"), // 일식
+    SOUP("Genrestrtsoup"), // 탕류
+    FASTFOOD("Genrestrtfastfood"), // 패스트푸드
+    SASH("Genrestrtsash"), // 생선회
+    BUFF("Genrestrtbuff"), // 뷔페식
+    FODTUCK("Resrestrtfodtuck") // 푸드 트럭
+        ;
+    private final String url;
+
+    OpenApiUrl(String url){
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return "https://openapi.gg.go.kr/" + url;
+    }
+}
