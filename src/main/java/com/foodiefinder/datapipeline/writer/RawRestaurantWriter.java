9f3dd1378bc31121f21d370166327292605cd71a
Hyeri Jang <chjang5621@naver.com>
2023-11-02 19:36:08 +0900

    feat : RawRestaurant 저장 기능 구현
    #1

---
+++ src/main/java/com/foodiefinder/datapipeline/writer/RawRestaurantWriter.java
@@ -0,0 +1,45 @@
+package com.foodiefinder.datapipeline.writer;
+
+import com.foodiefinder.datapipeline.writer.entity.RawRestaurant;
+import com.foodiefinder.datapipeline.writer.repository.RawRestaurantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RawRestaurantWriter implements ItemWriter<List<RawRestaurant>> {
+
+    private final RawRestaurantRepository rawRestaurantRepository;
+
+    @Override
+    @Transactional
+    public void write(List<RawRestaurant> restaurantList) {
+        saveAll(restaurantList);
+    }
+
+    @Transactional
+    List<RawRestaurant> saveAll(List<RawRestaurant> restaurantList) {
+        List<RawRestaurant> savedResult = new ArrayList<>();
+        for (RawRestaurant rawRestaurant : restaurantList) {
+            if (save(rawRestaurant) == null)
+                continue;
+            savedResult.add(rawRestaurant);
+        }
+        return savedResult;
+    }
+
+    private RawRestaurant save(RawRestaurant rawRestaurant) {
+        try {
+            return rawRestaurantRepository.save(rawRestaurant);
+        } catch (DataIntegrityViolationException e) {
+            // 요구사항 : unique 제약조건 위반 시 저장하지 않고 무시
+        }
+        return null;
+    }
+}
