aef48108180069895019692d035163e42b55517f
seoyoon047 <seoyoon047@gmail.com>
2023-11-04 05:26:39 +0900

    style: Response 클래스 이름 수정

--- src/main/java/com/foodiefinder/common/dto/Response.java
+++ src/main/java/com/foodiefinder/common/dto/Response.java
@@ -6,20 +6,20 @@ import org.springframework.http.HttpStatus;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ResponseDto<T> {
+public class Response<T> {
 
     private int status;
     private String message;
     private T data;
 
-    private ResponseDto(HttpStatus status, String message, T data) {
+    private Response(HttpStatus status, String message, T data) {
         this.status = status.value();
         this.message = message;
         this.data = data;
     }
 
-    public static <T> ResponseDto<T> success(T data) {
-        return new ResponseDto<>(HttpStatus.OK, "Success", data);
+    public static <T> Response<T> success(T data) {
+        return new Response<>(HttpStatus.OK, "Success", data);
     }
 
 }
