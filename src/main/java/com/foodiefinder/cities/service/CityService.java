93729eb79698e01c6e835699a94e12d8581370fb
seoyoon047 <seoyoon047@gmail.com>
2023-11-04 05:26:16 +0900

    refactor: getCities, getRawCities 메소드 수정

--- src/main/java/com/foodiefinder/cities/service/CityService.java
+++ src/main/java/com/foodiefinder/cities/service/CityService.java
@@ -1,12 +1,16 @@
 package com.foodiefinder.cities.service;
 
-import com.foodiefinder.common.dto.ResponseDto;
+
+import com.foodiefinder.cities.controller.response.CitesResponses;
+import com.foodiefinder.cities.domain.SggList;
+import com.foodiefinder.cities.factory.SggListFactory;
+import com.foodiefinder.common.dto.Response;
+import com.foodiefinder.common.exception.CustomException;
+import com.foodiefinder.common.exception.ErrorCode;
 import com.foodiefinder.datapipeline.writer.entity.Sgg;
 import com.foodiefinder.datapipeline.writer.repository.SggRepository;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,17 +22,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class CityService {
     private final SggRepository sggRepository;
+    private final SggListFactory sggListFactory;
 
-    public List<ResponseDto> getCities() {
-        List<Sgg> getCities = getRawCities();
-
-        return getCities.stream()
-                .map(ResponseDto::new)
-                .collect(Collectors.toList());
+    public Response<CitesResponses> getCities() {
+        List<Sgg> rawCitiesData = getRawCities();
+        SggList rawCities = sggListFactory.createSggList(rawCitiesData);
+        CitesResponses citesResponses = rawCities.toCitesResponses();
+        return Response.success(citesResponses);
     }
 
     private List<Sgg> getRawCities() {
-        return Optional.ofNullable(sggRepository.findAll())
-                .orElse(Collections.emptyList());
+        return Optional.of(sggRepository.findAll())
+                .orElseThrow(() -> new CustomException(ErrorCode.CITIES_DATA_NOT_FOUND));
     }
 }
