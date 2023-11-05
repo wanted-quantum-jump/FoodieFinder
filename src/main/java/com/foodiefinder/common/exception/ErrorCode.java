1ed3be375f32121f60981fb52f18907de40c201a
jeongjiwon <jungkt0x@naver.com>
2023-11-05 14:23:33 +0900

    feat: 유저 예외처리 추가
    유저의 아이디를 찾을 수 없을때 발생한느 USER_NOT_FOUND 에러 추가

--- src/main/java/com/foodiefinder/common/exception/ErrorCode.java
+++ src/main/java/com/foodiefinder/common/exception/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
 
     //User
     USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "U001", "이미 계정이 존재합니다."),
-
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U002", "유저를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
