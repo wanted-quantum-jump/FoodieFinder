b3ec498a5bded7de1edb41c76f68b5aa53a0430d
seoyoon047 <seoyoon047@gmail.com>
2023-11-06 03:45:53 +0900

    feat: RestaurantController 메소드 추가

--- src/main/java/com/foodiefinder/restaurants/controller/RestaurantsController.java
+++ src/main/java/com/foodiefinder/restaurants/controller/RestaurantsController.java
@@ -1,10 +1,17 @@
 package com.foodiefinder.restaurants.controller;
 
 import com.foodiefinder.common.dto.Response;
+import com.foodiefinder.restaurants.dto.RatingDto;
 import com.foodiefinder.restaurants.dto.RestaurantsResponse;
+import com.foodiefinder.restaurants.service.RatingService;
 import com.foodiefinder.restaurants.service.RestaurantsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/restaurants")
 public class RestaurantsController {
     private final RestaurantsService restaurantsService;
+    private final RatingService ratingService;
 
     @GetMapping
     public Response<RestaurantsResponse> getRestaurants(
@@ -22,5 +30,10 @@ public class RestaurantsController {
             , @RequestParam(name = "range", defaultValue = "1.0") double range
             , @RequestParam(name = "orderBy", defaultValue = "distance") String orderBy) {
         return this.restaurantsService.getRestaurants(lat, lon, range, orderBy);
+    }
+    @PostMapping("/{restaurantId}/ratings")
+    public Response<Void> createRating(@PathVariable Long restaurantId, @RequestBody RatingDto dto) {
+        return this.ratingService.createRating(restaurantId, dto);
+
     }
 }
