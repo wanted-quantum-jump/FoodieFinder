e622b8a744d5b128928e44bf91f4ce2103ef4cbe
jeongjiwon <jungkt0x@naver.com>
2023-11-05 15:59:53 +0900

    feat: 로그인 request 추가
    - 로그인 시 사용할 request dto를 생성

---
+++ src/main/java/com/foodiefinder/auth/dto/UserLoginRequest.java
@@ -0,0 +1,18 @@
+package com.foodiefinder.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+public class UserLoginRequest {
+
+    @NotBlank(message = "계정은 필수입력값입니다.")
+    private String account;
+
+    @NotBlank(message = "비밀번호는 필수입력값입니다.")
+    @Length(min = 10, message = "비밀번호는 10자 이상이여야합니다.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z!@#$%^&*()-_=+]).+$", message = "비밀번호는 숫자, 문자, 특수문자 중 2가지 이상을 포함해야 합니다.")
+    private String password;
+}
