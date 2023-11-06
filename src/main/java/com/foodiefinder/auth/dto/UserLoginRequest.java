0f109842ebb3d606a2222494664aab95b68de992
jeongjiwon <jungkt0x@naver.com>
2023-11-06 12:43:12 +0900

    test: 로그인, 인증인가 테스트 추가
    로그인, 권한 관련 간단한 테스트 코드 추가하였습니다.

--- src/main/java/com/foodiefinder/auth/dto/UserLoginRequest.java
+++ src/main/java/com/foodiefinder/auth/dto/UserLoginRequest.java
@@ -2,11 +2,15 @@ package com.foodiefinder.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Getter
-public class UserLoginRequest {
+@NoArgsConstructor
+public class
+UserLoginRequest {
 
     @NotBlank(message = "계정은 필수입력값입니다.")
     private String account;
@@ -15,4 +19,10 @@ public class UserLoginRequest {
     @Length(min = 10, message = "비밀번호는 10자 이상이여야합니다.")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z!@#$%^&*()-_=+]).+$", message = "비밀번호는 숫자, 문자, 특수문자 중 2가지 이상을 포함해야 합니다.")
     private String password;
+
+    @Builder
+    public UserLoginRequest(String account, String password) {
+        this.account = account;
+        this.password = password;
+    }
 }
