f2929c1ab182453b10f0e4eda9c477d2c758019a
Hyeri Jang <chjang5621@naver.com>
2023-11-08 16:40:48 +0900

    test : RestaurantRepository null 제약 조건 단위테스트 추가
    #61

--- src/test/java/com/foodiefinder/datapipeline/writer/repository/RestaurantRepositoryTest.java
+++ src/test/java/com/foodiefinder/datapipeline/writer/repository/RestaurantRepositoryTest.java
@@ -16,8 +16,6 @@ class RestaurantRepositoryTest {
     @Autowired
     RestaurantRepository restaurantRepository;
 
-    //TODO: null 조건에 대한 단위테스트 추가
-
     @Test
     @DisplayName("저장 성공")
     void fullFilledRestaurant() {
@@ -150,4 +148,180 @@ class RestaurantRepositoryTest {
         Assertions.assertThatNoException().isThrownBy(() -> restaurantRepository.save(restaurant2));
     }
 
+
+    @Test
+    @DisplayName("null 제약조건 : 위도는 null 일 수 없다.")
+    void checkNullLat() {
+        //given
+        Restaurant restaurant1 = Restaurant.builder()
+                .sigunName("화성시")
+                .businessPlaceName("푸드파인더 카페")
+                .businessStateName("영업")
+                .sanitationBusinessCondition("카페")
+                .roadAddress("경기도 화성시 반월동 869 (111호)")
+                .lotNumberAddress("경기도 화성시 영통로 59")
+                .zipCode(12345)
+                .latitude(null) //null
+                .longitude(127.12345)
+                .build();
+
+        //when
+        //than
+        Assertions.assertThatThrownBy(() -> restaurantRepository.save(restaurant1))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("null 제약조건 : 경도는 null 일 수 없다.")
+    void checkNullLon() {
+        //given
+        Restaurant restaurant1 = Restaurant.builder()
+                .sigunName("화성시")
+                .businessPlaceName("푸드파인더 카페")
+                .businessStateName("영업")
+                .sanitationBusinessCondition("카페")
+                .roadAddress("경기도 화성시 반월동 869 (111호)")
+                .lotNumberAddress("경기도 화성시 영통로 59")
+                .zipCode(12345)
+                .latitude(35.123)
+                .longitude(null)//null
+                .build();
+
+        //when
+        //than
+        Assertions.assertThatThrownBy(() -> restaurantRepository.save(restaurant1))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("null 제약조건 : 가게이름은 null 일 수 없다.")
+    void checkNullBusinessPlaceName() {
+        //given
+        Restaurant restaurant1 = Restaurant.builder()
+                .sigunName("화성시")
+                .businessPlaceName(null) //null
+                .businessStateName("영업")
+                .sanitationBusinessCondition("카페")
+                .roadAddress("경기도 화성시 반월동 869 (111호)")
+                .lotNumberAddress("경기도 화성시 영통로 59")
+                .zipCode(12345)
+                .latitude(35.123)
+                .longitude(123.12412)
+                .build();
+
+        //when
+        //than
+        Assertions.assertThatThrownBy(() -> restaurantRepository.save(restaurant1))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("null 제약조건 : 도로명주소는 null 일 수 없다.")
+    void checkNullRoadAddress() {
+        //given
+        Restaurant restaurant1 = Restaurant.builder()
+                .sigunName("화성시")
+                .businessPlaceName("푸드파인더 카페")
+                .businessStateName("영업")
+                .sanitationBusinessCondition("카페")
+                .roadAddress(null) //null
+                .lotNumberAddress("경기도 화성시 영통로 59")
+                .zipCode(12345)
+                .latitude(35.123)
+                .longitude(123.12412)
+                .build();
+
+        //when
+        //than
+        Assertions.assertThatThrownBy(() -> restaurantRepository.save(restaurant1))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("null 제약조건 : 영업상태는 null 일 수 없다.")
+    void checkNullStateName() {
+        //given
+        Restaurant restaurant1 = Restaurant.builder()
+                .sigunName("화성시")
+                .businessPlaceName("푸드파인더 카페")
+                .businessStateName(null)//null
+                .sanitationBusinessCondition("카페")
+                .roadAddress("경기도 화성시 반월동 869 (111호)")
+                .lotNumberAddress("경기도 화성시 영통로 59")
+                .zipCode(12345)
+                .latitude(35.123)
+                .longitude(123.12412)
+                .build();
+
+        //when
+        //than
+        Assertions.assertThatThrownBy(() -> restaurantRepository.save(restaurant1))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+
+    @DisplayName("null 제약조건 : 시군구는 null 일 수 없다.")
+    void checkNullSigunName() {
+        //given
+        Restaurant restaurant1 = Restaurant.builder()
+                .sigunName(null) //null
+                .businessPlaceName("푸드파인더 카페")
+                .businessStateName("영업")
+                .sanitationBusinessCondition("카페")
+                .roadAddress("경기도 화성시 반월동 869 (111호)")
+                .lotNumberAddress("경기도 화성시 영통로 59")
+                .zipCode(12345)
+                .latitude(35.123)
+                .longitude(123.12412)
+                .build();
+
+        //when
+        //than
+        Assertions.assertThatThrownBy(() -> restaurantRepository.save(restaurant1))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("null 제약조건 : 위생업태명은 null 일 수 없다.")
+    void checkNullSanitationBusinessCondition() {
+        //given
+        Restaurant restaurant1 = Restaurant.builder()
+                .sigunName("화성시")
+                .businessPlaceName("푸드파인더 카페")
+                .businessStateName("영업")
+                .sanitationBusinessCondition(null)//null
+                .roadAddress("경기도 화성시 반월동 869 (111호)")
+                .lotNumberAddress("경기도 화성시 영통로 59")
+                .zipCode(12345)
+                .latitude(23.4)
+                .longitude(127.12345)
+                .build();
+
+        //when
+        //than
+        Assertions.assertThatThrownBy(() -> restaurantRepository.save(restaurant1))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("null 제약조건 : 지번주소는 null 일 수 없다.")
+    void checkNullLotNumberAddress() {
+        //given
+        Restaurant restaurant1 = Restaurant.builder()
+                .sigunName("화성시")
+                .businessPlaceName("푸드파인더 카페")
+                .businessStateName("영업")
+                .sanitationBusinessCondition("카페")
+                .roadAddress("경기도 화성시 반월동 869 (111호)")
+                .lotNumberAddress(null) //null
+                .zipCode(12345)
+                .latitude(35.123)
+                .longitude(123.12412)
+                .build();
+
+        //when
+        //than
+        Assertions.assertThatThrownBy(() -> restaurantRepository.save(restaurant1))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
 }
