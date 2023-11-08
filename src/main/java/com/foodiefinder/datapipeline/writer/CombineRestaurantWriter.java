8133f6559309eab7240409e8d4d4d635ae455b33
BA <windy01999@gmail.com>
2023-11-09 02:33:12 +0900

    fix: response 만 업데이트 된 경우 에러 발생 수정

--- src/main/java/com/foodiefinder/datapipeline/writer/CombineRestaurantWriter.java
+++ src/main/java/com/foodiefinder/datapipeline/writer/CombineRestaurantWriter.java
@@ -14,11 +14,14 @@ public class CombineRestaurantWriter implements ItemWriter<CombineRestaurantProc
 
     @Override
     public void write(CombineRestaurantProcessorResultData input) {
-        if (input != null || !input.getRestaurants().isEmpty()) {
-            restaurantWriter.write(input.getRestaurants());
-            // Response, Restaurant 캐시 데이터 저장
+        if (input != null) {
+            if(!input.getRestaurants().isEmpty()) {
+                restaurantWriter.write(input.getRestaurants());
+                // Restaurant 캐시 데이터 저장
+                dataPipelineCacheRepository.setRestaurantCache(input.getRestaurants());
+            }
+            // input 이 null 은 아니지만 response body 만 변경되었으며 restaurant 는 변경이 안된 경우 response 만 캐시에 저장.
             dataPipelineCacheRepository.setResponseCache(input.getResponse());
-            dataPipelineCacheRepository.setRestaurantCache(input.getRestaurants());
         }
     }
 }
