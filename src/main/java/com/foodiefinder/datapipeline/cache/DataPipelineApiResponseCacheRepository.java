68de0789870287c34f7d07621ccb7d947f9d042e
BA <windy01999@gmail.com>
2023-11-14 21:50:44 +0900

    refactor: 주석 제거, 메서드 의미가 드러나도록 이름 변경

--- src/main/java/com/foodiefinder/datapipeline/cache/DataPipelineApiResponseCacheRepository.java
+++ src/main/java/com/foodiefinder/datapipeline/cache/DataPipelineApiResponseCacheRepository.java
@@ -3,7 +3,6 @@ package com.foodiefinder.datapipeline.cache;
 import com.foodiefinder.common.cache.CacheUtils;
 import com.foodiefinder.common.enums.CacheKeyPrefix;
 import com.foodiefinder.datapipeline.util.hash.HashGenerator;
-import jakarta.persistence.Cacheable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.RedisConnection;
@@ -18,43 +17,28 @@ public class DataPipelineApiResponseCacheRepository {
     private final long RESPONSE_EXPIRE_TIME = 5400;
     private final CacheUtils cacheUtils;
 
-    /**
-     * 레디스를 통해 해시 값으로 응답값 비교 후, 없으면 false, 존재하면 Expire 갱신
-     * @param response 응답 문자열
-     * @return 존재 여부
-     */
     public boolean hasResponseCache(String response) {
-        byte[] responseHash = getHash(response);
-        RedisConnection connection = cacheUtils.getConnection();
-        boolean isExist = connection.stringCommands().get(responseHash) != null;
-        connection.close();
-        return isExist;
+        byte[] responseHash = computeHashForResponse(response);
+        try(RedisConnection connection = cacheUtils.getConnection()) {
+            return connection.stringCommands().get(responseHash) != null;
+        }
     }
 
-    /**
-     * response 에 대한 해시값을 레디스에 저장
-     * @param response
-     * @return
-     */
-    public boolean inputResponseCache(String response) {
+    public void inputResponseCache(String response) {
         log.info("Response 해시, 캐싱 시작");
-        byte[] responseHash = getHash(response);
-        RedisConnection connection = cacheUtils.getConnection();
-        boolean set = connection.stringCommands()
-                .set(responseHash, "true".getBytes(),
-                        Expiration.seconds(RESPONSE_EXPIRE_TIME),
-                        RedisStringCommands.SetOption.UPSERT);
-        connection.close();
+        byte[] responseHash = computeHashForResponse(response);
+
+        try (RedisConnection connection = cacheUtils.getConnection()) {
+            connection.stringCommands()
+                    .set(responseHash, "true".getBytes(),
+                            Expiration.seconds(RESPONSE_EXPIRE_TIME),
+                            RedisStringCommands.SetOption.UPSERT);
+
+        }
         log.info("Response 해시, 캐싱 완료");
-        return set;
     }
 
-    /**
-     * 해시 값 생성
-     * @param response response 의 문자열
-     * @return 변환된 해시 값
-     */
-    private byte[] getHash(String response) {
+    private byte[] computeHashForResponse(String response) {
         return (CacheKeyPrefix.DATAPIPELINE_RESPONSE.getKeyPrefix() +
                 HashGenerator.calculateSHA256(response)).getBytes();
     }
